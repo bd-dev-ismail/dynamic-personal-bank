e0685d388647cf6f1275a3099a15e447d54b1782
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-16 08:14:05 +0600

    add evend listener & compair email-password

---
+++ js/login.js
@@ -0,0 +1,20 @@
+//Step-1. add click event handeler with submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //Step-2 Get the email address from email field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    //Step-3
+    //3.a-set the id on html element
+    //3.b-Get the element 
+    //3.c-Get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    //Danger: Dot Not Verify on the client side
+    //Step-4 Verify email & password and check valid user or not!!!!
+    if(email === 'sontan@bap.com' && password === 'screct'){
+        console.log('Valid User');
+    }
+    else{
+        console.log('Invalid User');
+    }
+});
