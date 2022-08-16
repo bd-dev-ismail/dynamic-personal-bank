c164be8990e68dcf32800dbc3991c92d6475b603
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-16 11:11:26 +0600

    added

--- js/deposit.js
+++ js/deposit.js
@@ -2,10 +2,27 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
 //Step-2 GEt the deposti amount from DepostiInput Field
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
-//Step-2 GEt the deposit Current amount
+    const newdepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newdepositAmountString);
+//Step-3 GEt the deposit Current amount
 //For non input (element other than input, textarea) use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal =  depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString =  depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+//Step-4 add number to set the deposit
+//set the deposit toal
+    depositTotalElement.innerText = currentDepositTotal;
+//Step-5 Get blance current total
+    const blanceTotalElement = document.getElementById('blance-total');
+    const previousBlanceTotalString = blanceTotalElement.innerText;
+    const previousBlanceTotal = parseFloat(previousBlanceTotalString);
+
+//Step-6 calculate total blance
+    const currentBlanceTotal = previousBlanceTotal + newDepositAmount;
+//set the blance total 
+    blanceTotalElement.innerText = currentBlanceTotal;
+  
+//Step-Extra Clear the deposit field
+    depositField.value = '';
 });
--- js/login.js
+++ js/login.js
@@ -2,16 +2,16 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
     //Step-2 Get the email address from email field
     const emailField = document.getElementById('user-email');
-    const email = emailField.value;
+    const email = emailField.value = '';
     //Step-3
     //3.a-set the id on html element
     //3.b-Get the element 
     //3.c-Get the value from the element
     const passwordField = document.getElementById('user-password');
-    const password = passwordField.value;
+    const password = passwordField.value = '';
     //Danger: Dot Not Verify on the client side
     //Step-4 Verify email & password and check valid user or not!!!!
-    if(email === 'sontan@bap.com' && password === 'screct'){
+    if(email === 'sontan@bap.com' && password === 'ratul'){
        window.location.href = 'bank.html';
     }
     else{
